0610e8bbb4428c9cd863a996a026417d8ae02751
moha_tangx <muhammadstangaza@gmail.com>
2025-06-10 03:09:33 +0100

    easier build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #if bundled remove bundled
-[ -d ./bin/ ] && rm -r ./bin/
+[ -f ./bin/licon ] && chmod +w ./bin/licon; rm  ./bin/licon
 
 # bundle app
 esbuild ./src/index.ts --bundle --format=esm --outfile=./bin/licon  --platform=node --minify &&
